e05ac475b8ddb14b03b06b938814793417b9739b
vivianaRodas <36738664+vivianaRodas@users.noreply.github.com>
2019-07-31 18:12:29 -0500

    cambios varios

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,9 +38,10 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Inicio', 'url' => ['/site/index']],
+            ['label' => 'Terceros', 'url' => ['/terceros/create']],
+            ['label' => 'Contratos', 'url' => ['/site/contact']], 
+			['label' => 'Extracto Contratos', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
