cc37f120c54803aa8ee0038e3d0249aeeeb481fc
vivianaRodas <36738664+vivianaRodas@users.noreply.github.com>
2019-08-05 15:55:31 -0500

    index - logo

--- views/site/index.php
+++ views/site/index.php
@@ -1,17 +1,28 @@
 <?php
 
 use yii\helpers\Html;
-
 /* @var $this yii\web\View */
 
 $this->title = 'Icontroltrans';
 ?>
 <div class="site-index">
 
-   
+    <div class="jumbotron">
+       
+		
+    </div>
+
     <div class="body-content">
 
-        <?php echo Html::img('@web/images/hys.png') ?>
+        <div class="row">
+            <div class="col-lg-4">
+            </div>
+            <div class="col-lg-4">
+                <?php echo Html::img('@web/images/logo.png', ['width' => '400px', 'height' => '300px']) ?>
+            </div>
+            <div class="col-lg-4">
+            </div>
+        </div>
 
     </div>
 </div>
